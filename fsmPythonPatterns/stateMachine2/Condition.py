aab0c542a7219aeb59aaba092d38e3fa8ba22d41
jaquinonesg <jaquinonesg@unal.edu.co>
2017-07-08 11:54:58 -0500

    gitignore

---
+++ fsmPythonPatterns/stateMachine2/Condition.py
@@ -0,0 +1,5 @@
+#Condition function object for state Machine
+
+class Condition:
+    def condition(self, input):
+        assert 0, "condition() not implemented"
---
+++ fsmPythonPatterns/stateMachine2/Transition.py
@@ -0,0 +1,5 @@
+#Transition function object for state machine
+
+class Transition:
+    def transition(self, input):
+        assert 0, "transition() not implemented"
