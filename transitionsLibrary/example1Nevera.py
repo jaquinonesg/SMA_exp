1c9b9b09f7c27d2beb251c50867eaf492fbce689
jaquinonesg <jaquinonesg@unal.edu.co>
2017-09-25 17:02:50 -0500

    fridge example base

---
+++ transitionsLibrary/example1Nevera.py
@@ -0,0 +1,33 @@
+import os, sys, inspect
+
+cmd_folder = os.path.realpath(
+    os.path.dirname(
+        os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0])))
+
+if cmd_folder not in sys.path:
+    sys.path.insert(0, cmd_folder)
+    
+from transitions import *
+from transitions.extensions import GraphMachine
+
+class Fridge(object):
+    
+    # graph object is created by the machine
+    def show_graph(self, **kwargs):
+        self.get_graph(**kwargs).draw('matter.png', prog='dot')
+
+transitions = [
+    { 'trigger': 'high_temperature', 'source': 'turn_off', 'dest': 'turn_on' },
+    { 'trigger': 'temperature_is_fine', 'source': 'turn_on', 'dest': 'turn_off'}
+]
+states=['turn_on','turn_off']
+
+model = Fridge()
+machine = GraphMachine(model=model, 
+                       states=states, 
+                       transitions=transitions,
+                       initial='turn_off',
+                       show_auto_transitions=True,
+                       title="Fridge",
+                       show_conditions=True)
+model.show_graph()
