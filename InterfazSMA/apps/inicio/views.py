f9c8c8dceb681b72250061ea92f779d63a0a5e02
Jaquinonesg <jaquinonesg@gmail.com>
2017-10-06 11:18:10 -0500

    Inicio despejado

--- InterfazSMA/apps/inicio/views.py
+++ InterfazSMA/apps/inicio/views.py
@@ -1,5 +1,11 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render
+from django.views.defaults import page_not_found
+
+def error_404(request):
+    nombre_template = '404.html'
+ 
+    return page_not_found(request, template_name=nombre_template)
 
 class Inicio(TemplateView):
     template_name = 'inicio/index.html'
--- InterfazSMA/tlon/urls.py
+++ InterfazSMA/tlon/urls.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from django.conf.urls import url, include
 from django.conf.urls.static import static
 
+from django.conf.urls import handler404
+from apps.inicio.views import error_404
+ 
+handler404 = error_404
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('apps.inicio.urls')),
@@ -10,4 +15,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
