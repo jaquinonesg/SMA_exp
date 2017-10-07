ee9ed52b7bb4c3d6d3f841e4c1c9a5165a860e8c
juan <jumcorredorro@unal.edu.co>
2017-10-06 23:50:37 -0500

    final

--- InterfazSMA/apps/inicio/views.py
+++ InterfazSMA/apps/inicio/views.py
@@ -96,6 +96,7 @@ class Agentes(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(Agentes, self).get_context_data(**kwargs)
+        context['agentes']=Agente.objects.all()
         return context
 
 class Documentacion(TemplateView):
