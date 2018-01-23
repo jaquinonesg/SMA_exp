ff47d93ccec7f540ff162917a15ae7bdd92257eb
jaquinonesg <jaquinonesg@unal.edu.co>
2018-01-23 13:25:57 -0500

    first raspbian full functional

--- scriptsRaspberry/sensors.py
+++ scriptsRaspberry/sensors.py
@@ -4,45 +4,43 @@ import socket, datetime, gps, sys, time, Adafruit_DHT, requests
 
 session = gps.gps("localhost", "2947")
 session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
-x= 1
-
-while x == 1:
- report = session.next()
- if report['class'] == 'TPV':
-  if hasattr(report, 'time'):
-   print 'Hora:      ' + str(report.tim1e)
-  if hasattr(report, 'lat'):
-   print 'Latitud:   ' + str(report.lat)
-   lat = str(report.lat)
-  if hasattr(report, 'lon'):
-   print 'Longitud:  ' + str(report.lon)
-   lon = str(report.lon)
-  if hasattr(report, 'speed'):
-   print 'Velocidad: ' + str(report.speed)
-  if hasattr(report, 'track'):
-   print 'Rumbo:     ' + str(report.track)
-  if hasattr(report, 'head'):
-   print report.head
-  x= 0
-
-# Configuracion del tipo de sensor DHT
 sensor = Adafruit_DHT.DHT11
-
-# Configuracion del puerto GPIO al cual esta conectado  (GPIO 23)
 pin = 23
+lat = 0
+lon = 0
+time = 0
 
-# Intenta ejecutar las siguientes instrucciones, si falla va a la instruccion except
+def gps():
+ x = 1
+ while x == 1:
+  report = session.next()
+  if report['class'] == 'TPV':
+   if hasattr(report, 'time'):
+    print 'Hora:      ' + str(report.time)
+    global time
+    time = str(report.time)
+   if hasattr(report, 'lat'):
+    print 'Latitud:   ' + str(report.lat)
+    global lat
+    lat = str(report.lat)
+   if hasattr(report, 'lon'):
+    print 'Longitud:  ' + str(report.lon)
+    global lon
+    lon = str(report.lon)
+   if hasattr(report, 'speed'):
+    print 'Velocidad: ' + str(report.speed)
+   if hasattr(report, 'track'):
+    print 'Rumbo:     ' + str(report.track)
+   if hasattr(report, 'head'):
+    print report.head
+   x= 0
+ 
 try:
 	# Ciclo principal infinito
 	while True:
-		# Obtiene la humedad y la temperatura desde el sensor
 		humedad, temperatura = Adafruit_DHT.read_retry(sensor, pin)
-		# Imprime en la consola las variables temperatura y humedad con un decimal
-		
-		r = requests.post("http://www.bitsobet.com/maps/", data={'temp': temperatura, 'hum': humedad, 'longitud' : lon, 'latitud' : lat, 'humsuelo' : 0, 'precipitacion' : 6, 'datemed' : '2018-02-02T12:55:29.000Z'})
-		#print("El servicio web no puede ser alcanzado")
-		# Duerme 10 segundos
-		time.sleep(5)
+		gps()
+		r = requests.post("http://www.bitsobet.com/maps/", data={'temp': temperatura, 'hum': humedad, 'longitud' : lon, 'latitud' : lat, 'humsuelo' : 0, 'precipitacion' : 6, 'datemed' : time})
 
 # Se ejecuta en caso de que falle alguna instruccion dentro del try
 except Exception,e:
