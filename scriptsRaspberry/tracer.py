86c71ec648212784d55daa36eb4b7ce108350efe
jaquinonesg <jaquinonesg@unal.edu.co>
2018-01-23 23:11:34 -0500

    traceroute fixes

--- scriptsRaspberry/tracer.py
+++ scriptsRaspberry/tracer.py
@@ -2,6 +2,7 @@ import sys
 import socket
  
 def traceroute(hostname, port, max_hops):
+    result = []
     destination = socket.gethostbyname(hostname)
     print "target %s" % hostname
  
@@ -9,7 +10,7 @@ def traceroute(hostname, port, max_hops):
     udp = socket.getprotobyname('udp')
     ttl = 1
  
-    while True:
+    while ttl < 50:
         recvsock = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp)
         sendsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, udp)
         sendsock.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
@@ -36,10 +37,14 @@ def traceroute(hostname, port, max_hops):
         else:
             currenthost = "*"
         print "%dt%s" % (ttl, currenthost)
- 
+        result.append(ttl)
+        result.append(currenthost)
         ttl += 1
         if currentaddr == destination or ttl > max_hops:
             break
+        
+        
+    return result
  
-if __name__ == "__main__":
-    traceroute(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+result = traceroute('google.com', 50, 30)
+print(result)
